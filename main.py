588154d51623054c770717d5c28b1f4ad0206c42
ccrsxx <aminrisal@gmail.com>
2024-01-25 19:53:06 +0700

    feat: improve main script to allow updating service

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import os
 import subprocess
 
+IGNORE_SERVICES_UPDATE = [
+    'nextcloud'
+]
+
 def main() -> None:
     services = os.listdir('src')
 
+    running_from_host = os.getenv('SSH_TTY') is None
+
     for service in services:
         service_path  = os.path.join('src', service, 'compose.yaml')
+
+        allow_service_update = service not in IGNORE_SERVICES_UPDATE and running_from_host
+
+        if allow_service_update:
+            subprocess.call(['docker', 'compose', '-f', service_path, 'pull'])
+
         subprocess.call(['docker', 'compose', '-f', service_path, 'up', '-d'])
 
 if __name__ == '__main__':
